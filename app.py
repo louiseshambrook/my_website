de3dde1f78bacd1975026298655fb192bf127adf
louiseshambrook <louisegreen18@gmail.com>
2023-09-15 16:14:14 +0100

    basic blog site built

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 from flask import Flask, render_template
+from article_data import Articles
+
 app = Flask(__name__)
 
+Articles = Articles()
+
 @app.route('/')
-def home():
-   return render_template('/pages/index.html')
+def index():
+   return render_template('home.html')
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
+
+@app.route('/articles')
+def articles():
+   return render_template('articles.html', articles=Articles)
+
+@app.route('/articles/<string:id>/')
+def display_article(id):
+   return render_template('article.html', id=id)
+
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
---
+++ article_data.py
@@ -0,0 +1,26 @@
+def Articles():
+    articles = [       
+        {
+            'id': 1,
+            'title': 'First article',
+            'body': "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lectus mi, hendrerit nec massa mollis, ultricies bibendum neque. Integer vestibulum accumsan rhoncus",
+            'author': 'Sophia Iroegbu',
+            'date_created': '26-01-2022'
+        },
+        {
+            'id': 2,
+            'title': 'Second article',
+            'body': 'lNam quis diam sed mauris congue venenatis at et est. Phasellus aliquet justo velit, et lacinia enim venenatis id. Integer pulvinar sollicitudin tempor. Integer in nibh rhoncus, aliquet felis eget, vehicula felis.',
+            'author': 'John Doe',
+            'date_created': '25-01-2022'
+        },
+        {
+            'id': 3,
+            'title': 'Third article',
+            'body': 'Aenean vehicula arcu id quam dapibus, luctus laoreet est vestibulum. Nulla eget metus lectus. Phasellus nisi nibh, egestas nec venenatis pellentesque, gravida id augue.',
+            'author': 'Mary James',
+            'date_created': '26-01-2022'
+        },
+    ]
+
+    return articles
